bbcfc11a496933bfd38d855afac0ff8bc0dbaa24
FlashbangCat <eundor03@naver.com>
2025-04-16 19:25:33 +0900

    Main

--- src/calculatingmachine/Main.java
+++ src/calculatingmachine/Main.java
@@ -1,7 +1,17 @@
 package calculatingmachine;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int intbox1 = sc.nextInt();
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int intbox2 = sc.nextInt();
+
+        int val = intbox1 + intbox2;
+        System.out.println("값: " + val);
 
     }
 }
