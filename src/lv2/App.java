729e759d7c7fb75d5d5d133d5bf783708616b7f2
FlashbangCat <eundor03@naver.com>
2025-04-22 13:34:32 +0900

    lv.2 연산값 가져오기와 캡슐화 완성

--- src/lv2/App.java
+++ src/lv2/App.java
@@ -1,7 +1,50 @@
 package lv2;
 
+import java.util.Scanner;
+import java.util.LinkedList;
+
 public class App {
+
     public static void main(String[] args) {
 
+        Calculator cal = new Calculator();
+        LinkedList<Integer> list = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
+
+        boolean re = true;
+
+        while (re) {
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int val1 = sc.nextInt();
+            cal.setVal1(val1);
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int val2 = sc.nextInt();
+            cal.setVal2(val2);
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char oper = sc.next().charAt(0);
+            cal.setOper(oper);
+
+            try {
+                int result = cal.calculate();
+                System.out.println("값 = " + result);
+            } catch (ArithmeticException e){
+                System.out.println("0으로 나눌수 없습니다.");
+                continue;
+            } catch (NullPointerException e){
+                System.out.println("결과값이 NULL 일 수 없습니다.");
+                continue;
+            }
+
+            void 
+
+
+            //무한 탈출
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            String rebox = sc.next();
+            if (rebox.equals("exit")) {
+                re = false;
+            }
+        }
     }
 }
--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,4 +1,44 @@
 package lv2;
 
 public class Calculator {
+    private int val1;
+    private int val2;
+    private char oper;
+
+
+    public int calculate() {
+        Integer result = null;
+        switch (oper) {
+            case '+' -> result = val1 + val2;
+            case '-' -> result = val1 - val2;
+            case '*' -> result = val1 * val2;
+            case '/' -> result = val1 / val2;
+            default -> System.out.println("유효하지 않은 연산자 입니다.");
+        }
+        return result;
+    }
+
+    public int getVal1() {
+        return val1;
+    }
+
+    public void setVal1(int val1) {
+        this.val1 = val1;
+    }
+
+    public int getVal2() {
+        return val2;
+    }
+
+    public void setVal2(int val2) {
+        this.val2 = val2;
+    }
+
+    public char getOper() {
+        return oper;
+    }
+
+    public void setOper(char oper) {
+        this.oper = oper;
+    }
 }
