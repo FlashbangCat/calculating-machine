43cf573db15920c15e49eb866d8517baed386448
FlashbangCat <eundor03@naver.com>
2025-04-23 10:17:44 +0900

    lv.2 연산값 가져오기와 캡슐화 완성

--- src/lv2/App.java
+++ src/lv2/App.java
@@ -1,14 +1,13 @@
 package lv2;
 
-import java.util.Scanner;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
 
         Calculator cal = new Calculator();
-        LinkedList<Integer> list = new LinkedList<>();
         Scanner sc = new Scanner(System.in);
 
         boolean re = true;
@@ -26,24 +25,38 @@ public class App {
             cal.setOper(oper);
 
             try {
-                int result = cal.calculate();
+                Integer result = cal.calculate();
                 System.out.println("값 = " + result);
+                cal.list.addLast(result);
+                System.out.println(cal.getList());
+
             } catch (ArithmeticException e){
                 System.out.println("0으로 나눌수 없습니다.");
                 continue;
+
             } catch (NullPointerException e){
                 System.out.println("결과값이 NULL 일 수 없습니다.");
                 continue;
             }
 
-            void 
-
-
             //무한 탈출
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            System.out.println("아무 키를 입력해주세요. (exit 입력 시 종료), (delete 입력시 삭제), (set 입력시 수정)");
             String rebox = sc.next();
             if (rebox.equals("exit")) {
                 re = false;
+
+            } else if (rebox.equals("delete")){
+                System.out.println("삭제되었습니다.");
+                cal.remove();
+                System.out.println(cal.getList());
+
+            } else  if (rebox.equals("set")){
+                System.out.print("몇번째 배열의 값을 바꿀지 선택해 주세요: ");
+                int posi = sc.nextInt();
+                System.out.print("무슨 값으로 바꿀지 작성해 주세요:");
+                int cr = sc.nextInt();
+                cal.setList(posi,cr);
+                System.out.println(cal.getList());
             }
         }
     }
--- src/lv2/Calculator.java
+++ src/lv2/Calculator.java
@@ -1,10 +1,14 @@
 package lv2;
 
+import java.util.LinkedList;
+
 public class Calculator {
+
+    LinkedList<Integer> list = new LinkedList<>();
+
     private int val1;
     private int val2;
     private char oper;
-
 
     public int calculate() {
         Integer result = null;
@@ -16,6 +20,10 @@ public class Calculator {
             default -> System.out.println("유효하지 않은 연산자 입니다.");
         }
         return result;
+    }
+
+    public void remove() {
+        list.removeFirst();
     }
 
     public int getVal1() {
@@ -40,5 +48,17 @@ public class Calculator {
 
     public void setOper(char oper) {
         this.oper = oper;
+    }
+
+    public LinkedList<Integer> getList() {
+        return list;
+    }
+
+    public void setList(int posi,int cr) {
+        if (posi > 0 && posi <= list.size()){
+            list.set(posi-1,cr);
+        } else {
+            System.out.println("자리가 잘못되었습니다.");
+        }
     }
 }
